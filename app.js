d686f4cd17237fcfec5a8509741186b15c4a3bb1
carpinellx <carpinellx@gmail.com>
2024-09-05 18:37:00 -0300

    Adds dados in js

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log(dados);
+function pesquisar() {
+    // Obtém a seção onde os resultados da pesquisa serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+  
+    // Inicializa uma string vazia para armazenar os resultados formatados
+    let resultados = "";
+  
+    // Itera sobre cada dado na lista de dados (assumindo que 'dados' é um array)
+    for (let dado of dados) { 
+      // Constrói o HTML para cada resultado, formatando o título, descrição e link
+      resultados += `
+        <div class="item-resultado">
+          <h2>
+            <a href="https://www.instagram.com/acmilan/" target="_blank">${dado.titulo}</a>
+          </h2>
+          <p class="item-resultado">${dado.descricao}</p>
+          <a href=${dado.link} target="_blank">Mais Informações</a>
+        </div>
+      `;
+    }
+  
+    // Atribui o HTML construído à seção de resultados
+    section.innerHTML = resultados;
+  }
