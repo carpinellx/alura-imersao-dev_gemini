d972abe53e07023912b2d0285bee7beb38b9dd92
carpinellx <carpinellx@gmail.com>
2024-09-05 17:42:42 -0300

    Adds list for search

---
+++ dados.js
@@ -0,0 +1,26 @@
+let dados = [
+    {
+        titulo: "AC Milan",
+        Pais: "Itália",
+        localizacao: "Milao",
+        Estadio: "San Siro",
+        descricao: "O Milan AC é um dos clubes de futebol mais históricos e prestigiados da Itália e do mundo. Fundado em 1899, o clube rossonero possui uma vasta galeria de troféus, incluindo inúmeros títulos nacionais e internacionais. Com uma torcida apaixonada e uma rica tradição, o Milan já teve em seu elenco grandes jogadores que marcaram época no futebol mundial. O clube é conhecido por sua elegância dentro e fora de campo, e por sua rivalidade histórica com a Internazionale, em um dos maiores clássicos do futebol mundial.",
+        link: "https://www.transfermarkt.com.br/ac-mailand/startseite/verein/5"
+    },
+    {
+        titulo: "Arsenal FC",
+        Pais: "Inglaterra",
+        localizacao: "Londres",
+        Estadio: "Estádio Emirates",
+        descricao: "O Arsenal FC é um dos clubes de futebol mais populares da Inglaterra. Fundado em 1886, o clube tem uma longa história e uma base de torcedores apaixonados em todo o mundo. Conhecido por seu estilo de jogo técnico e elegante, o Arsenal já conquistou diversos títulos nacionais e internacionais, incluindo vários campeonatos ingleses e a UEFA Champions League.",
+        link: "https://www.transfermarkt.com.br/arsenal/startseite/verein/11"
+    },
+    {
+        titulo: "Aston Villa FC",
+        Pais: "Inglaterra",
+        localizacao: "Birmingham",
+        Estadio: "Villa Park",
+        descricao: "O Aston Villa FC é um dos clubes de futebol mais tradicionais da Inglaterra. Fundado em 1874, o clube tem uma rica história e uma base de torcedores apaixonados. Conhecido por sua rivalidade com o Birmingham City, o Aston Villa já conquistou diversos títulos nacionais, incluindo sete campeonatos ingleses.",
+        link: "https://www.transfermarkt.com.br/aston-villa/startseite/verein/60"
+    }
+];
