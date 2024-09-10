b9e36e5da9560b9051309c8401765a4f6bd42d1c
carpinellx <carpinellx@gmail.com>
2024-09-09 22:35:02 -0300

    add search and error messages

--- app.js
+++ app.js
@@ -2,30 +2,51 @@ function pesquisar() {
     // Obtém a seção onde os resultados da pesquisa serão exibidos
     let section = document.getElementById("resultados-pesquisa");
   
+  let campoPesquisa =  document.getElementById("campo-pesquisa").value
+
+  //se campoPesquisa for uma string sem nada
+  if (campoPesquisa == ""){
+    section.innerHTML = "<span class='mensagem-erro'>Busca vazia</span>"
+    return
+  } 
+
+  campoPesquisa = campoPesquisa.toLowerCase()
+
     // Inicializa uma string vazia para armazenar os resultados formatados
     let resultados = "";
+    let titulo = "";
+    let descricao = "";
   
     // Itera sobre cada dado na lista de dados (assumindo que 'dados' é um array)
     for (let dado of dados) { 
-      // Constrói o HTML para cada resultado, formatando o título, descrição e link
+      titulo = dado.titulo.toLowerCase()
+      descricao = dado.descricao.toLocaleLowerCase()
+      //se titulo includer campoPesquisa 
+      if(titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa)) {
+              // Constrói o HTML para cada resultado, formatando o título, descrição e link
       resultados += `
-        <div class="item-resultado">
-          <h2>
-            <a href="#" style="color: ${dado.corPrincipal}" target="_blank">${dado.titulo}</a>
-          </h2>
-          <div class="item-resultado1">
-          <p class="item-info">De: ${dado.localizacao}, ${dado.pais} </p>
-          <p> Titulo(s): ${dado.titulos} </p>
-          <p> Desde: ${dado.fundado} </p>
-          <p> Estadio: ${dado.estadio} </p>
-          <p> Maior rival: ${dado.rival}
-          </div>
-          <p class="item-resultado">${dado.descricao}</p>
-          <a href=${dado.link} target="_blank">Mais Informações</a>
+      <div class="item-resultado">
+        <h2>
+          <a href="#" style="color: ${dado.corPrincipal}" target="_blank">${dado.titulo}</a>
+        </h2>
+        <div class="item-resultado1">
+        <p class="item-info">De: ${dado.localizacao}, ${dado.pais} </p>
+        <p> Titulo(s): ${dado.titulos} </p>
+        <p> Desde: ${dado.fundado} </p>
+        <p> Estadio: ${dado.estadio} </p>
+        <p> Maior rival: ${dado.rival}
         </div>
-      `;
+        <p class="item-resultado">${dado.descricao}</p>
+        <a href=${dado.link} target="_blank">Mais Informações</a>
+      </div>
+    `;
+      }
     }
-  
+
+    if(!resultados) {
+      resultados = "<span class='mensagem-erro'>Nada foi encontrado. Você não buscou por um time.</span>"
+    }
+
     // Atribui o HTML construído à seção de resultados
     section.innerHTML = resultados;
   }
